8accd4943f2618e6eda9f0ef0f6a9b9be280c5d3
davidsastresas <davidsastresas@gmail.com>
2023-05-07 18:42:20 +0200

    Vehicle: remove debug messages

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -4362,7 +4362,7 @@ void Vehicle::setGimbalRcTargeting()
 
 void Vehicle::sendGimbalManagerPitchYawFlags(uint32_t flags)
 {
-    qDebug() << "flags to send: " << flags;
+    // qDebug() << "flags to send: " << flags;
 
     sendMavCommand(
                 _defaultComponentId,
