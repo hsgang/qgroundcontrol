8875472bfd3aa1a0aa2fd898aa189d5fe77661eb
hsgang <hsgangmain@naver.com>
2023-05-25 10:44:19 +0900

    Update Landingtarget Fact

--- src/Vehicle/VehicleLandingTargetFactGroup.cc
+++ src/Vehicle/VehicleLandingTargetFactGroup.cc
@@ -1,16 +1,46 @@
 #include "VehicleLandingTargetFactGroup.h"
 #include "Vehicle.h"
 
-const char* VehicleLandingTargetFactGroup::_angleXFactName = "angleX";
-const char* VehicleLandingTargetFactGroup::_angleYFactName = "angleY";
+const char* VehicleLandingTargetFactGroup::_targetNumFactName       = "targetNum";
+const char* VehicleLandingTargetFactGroup::_angleXFactName          = "angleX";
+const char* VehicleLandingTargetFactGroup::_angleYFactName          = "angleY";
+const char* VehicleLandingTargetFactGroup::_distanceFactName        = "distance";
+const char* VehicleLandingTargetFactGroup::_sizeXFactName           = "sizeX";
+const char* VehicleLandingTargetFactGroup::_sizeYFactName           = "sizeY";
+const char* VehicleLandingTargetFactGroup::_xFactName               = "x";
+const char* VehicleLandingTargetFactGroup::_yFactName               = "y";
+const char* VehicleLandingTargetFactGroup::_zFactName               = "z";
+const char* VehicleLandingTargetFactGroup::_qFactName               = "q";
+const char* VehicleLandingTargetFactGroup::_typeFactName            = "type";
+const char* VehicleLandingTargetFactGroup::_positionValidFactName   = "postionValid";
 
 VehicleLandingTargetFactGroup::VehicleLandingTargetFactGroup(QObject* parent)
     : FactGroup             (200, ":/json/Vehicle/LandingTargetFactGroup.json", parent)
-    , _angleXFact     (0, _angleXFactName,      FactMetaData::valueTypeDouble)
-    , _angleYFact     (0, _angleYFactName,      FactMetaData::valueTypeDouble)
+    , _targetNumFact        (0, _targetNumFactName,     FactMetaData::valueTypeUint8)
+    , _angleXFact           (0, _angleXFactName,        FactMetaData::valueTypeDouble)
+    , _angleYFact           (0, _angleYFactName,        FactMetaData::valueTypeDouble)
+    , _distanceFact         (0, _distanceFactName,      FactMetaData::valueTypeDouble)
+    , _sizeXFact            (0, _sizeXFactName,         FactMetaData::valueTypeDouble)
+    , _sizeYFact            (0, _sizeYFactName,         FactMetaData::valueTypeDouble)
+    , _xFact                (0, _xFactName,             FactMetaData::valueTypeDouble)
+    , _yFact                (0, _yFactName,             FactMetaData::valueTypeDouble)
+    , _zFact                (0, _zFactName,             FactMetaData::valueTypeDouble)
+    , _qFact                (0, _qFactName,             FactMetaData::valueTypeDouble)
+    , _typeFact             (0, _typeFactName,          FactMetaData::valueTypeUint8)
+    , _positionValidFact    (0, _positionValidFactName, FactMetaData::valueTypeUint8)
 {
-    _addFact(&_angleXFact,        _angleXFactName);
-    _addFact(&_angleYFact,        _angleYFactName);
+    _addFact(&_targetNumFact,       _targetNumFactName);
+    _addFact(&_angleXFact,          _angleXFactName);
+    _addFact(&_angleYFact,          _angleYFactName);
+    _addFact(&_distanceFact,        _distanceFactName);
+    _addFact(&_sizeXFact,           _sizeXFactName);
+    _addFact(&_sizeYFact,           _sizeYFactName);
+    _addFact(&_xFact,               _xFactName);
+    _addFact(&_yFact,               _yFactName);
+    _addFact(&_zFact,               _zFactName);
+    _addFact(&_qFact,               _qFactName);
+    _addFact(&_typeFact,            _typeFactName);
+    _addFact(&_positionValidFact,   _positionValidFactName);
 }
 
 void VehicleLandingTargetFactGroup::handleMessage(Vehicle* /* vehicle */, mavlink_message_t& message)
@@ -29,6 +59,9 @@ void VehicleLandingTargetFactGroup::_handleLandingTarget(mavlink_message_t &mess
     mavlink_landing_target_t landingtarget;
     mavlink_msg_landing_target_decode(&message, &landingtarget);
 
+    targetNum()->setRawValue(landingtarget.target_num);
     angleX()->setRawValue(landingtarget.angle_x);
     angleY()->setRawValue(landingtarget.angle_y);
+    distance()->setRawValue(landingtarget.distance);
+    positionValid()->setRawValue(landingtarget.position_valid);
 }
--- src/Vehicle/VehicleLandingTargetFactGroup.h
+++ src/Vehicle/VehicleLandingTargetFactGroup.h
@@ -21,21 +21,61 @@ class VehicleLandingTargetFactGroup : public FactGroup
 public:
     VehicleLandingTargetFactGroup(QObject* parent = nullptr);
 
-    Q_PROPERTY(Fact* angleX       READ angleX       CONSTANT)
-    Q_PROPERTY(Fact* angleY       READ angleY        CONSTANT)
+    Q_PROPERTY(Fact* targetNum      READ targetNum          CONSTANT);
+    Q_PROPERTY(Fact* angleX         READ angleX             CONSTANT);
+    Q_PROPERTY(Fact* angleY         READ angleY             CONSTANT);
+    Q_PROPERTY(Fact* distance       READ distance           CONSTANT);
+    Q_PROPERTY(Fact* sizeX          READ sizeX              CONSTANT);
+    Q_PROPERTY(Fact* sizeY          READ sizeY              CONSTANT);
+    Q_PROPERTY(Fact* x              READ x                  CONSTANT);
+    Q_PROPERTY(Fact* y              READ y                  CONSTANT);
+    Q_PROPERTY(Fact* z              READ z                  CONSTANT);
+    Q_PROPERTY(Fact* q              READ q                  CONSTANT);
+    Q_PROPERTY(Fact* type           READ type               CONSTANT);
+    Q_PROPERTY(Fact* positionValid  READ positionValid      CONSTANT);
 
-    Fact* angleX      () { return &_angleXFact; }
-    Fact* angleY      () { return &_angleYFact; }
+    Fact* targetNum     () { return &_targetNumFact; }
+    Fact* angleX        () { return &_angleXFact; }
+    Fact* angleY        () { return &_angleYFact; }
+    Fact* distance      () { return &_distanceFact; }
+    Fact* sizeX         () { return &_sizeXFact; }
+    Fact* sizeY         () { return &_sizeYFact; }
+    Fact* x             () { return &_xFact; }
+    Fact* y             () { return &_yFact; }
+    Fact* z             () { return &_zFact; }
+    Fact* q             () { return &_qFact; }
+    Fact* type          () { return &_typeFact; }
+    Fact* positionValid () { return &_positionValidFact; }
 
     // Overrides from FactGroup
     void handleMessage(Vehicle* vehicle, mavlink_message_t& message);
 
+    static const char* _targetNumFactName;
     static const char* _angleXFactName;
     static const char* _angleYFactName;
+    static const char* _distanceFactName;
+    static const char* _sizeXFactName;
+    static const char* _sizeYFactName;
+    static const char* _xFactName;
+    static const char* _yFactName;
+    static const char* _zFactName;
+    static const char* _qFactName;
+    static const char* _typeFactName;
+    static const char* _positionValidFactName;
 
 private:
     void _handleLandingTarget (mavlink_message_t &message);
 
+    Fact _targetNumFact;
     Fact _angleXFact;
     Fact _angleYFact;
+    Fact _distanceFact;
+    Fact _sizeXFact;
+    Fact _sizeYFact;
+    Fact _xFact;
+    Fact _yFact;
+    Fact _zFact;
+    Fact _qFact;
+    Fact _typeFact;
+    Fact _positionValidFact;
 };
