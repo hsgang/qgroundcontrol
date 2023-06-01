39bfc0711541b34db80ae77bfc614eb62ef63819
hsgang <hsgangmain@naver.com>
2023-06-01 18:03:55 +0900

    Update TunnelingDataFact

--- src/Vehicle/TunnelingDataFactGroup.cc
+++ src/Vehicle/TunnelingDataFactGroup.cc
@@ -5,29 +5,32 @@
 
 const char* TunnelingDataFactGroup::_temperatureFactName =      "temperature";
 const char* TunnelingDataFactGroup::_humidityFactName =         "humidity";
+const char* TunnelingDataFactGroup::_pressureFactName =         "pressure";
 const char* TunnelingDataFactGroup::_windspeedFactName =        "windSpeed";
 const char* TunnelingDataFactGroup::_winddirectionFactName =    "windDirection";
-const char* TunnelingDataFactGroup::_pm2_5FactName =            "pm2_5";
+const char* TunnelingDataFactGroup::_pm1p0FactName =            "pm1p0";
+const char* TunnelingDataFactGroup::_pm2p5FactName =            "pm2p5";
 const char* TunnelingDataFactGroup::_pm10FactName =             "pm10";
-const char* TunnelingDataFactGroup::_radiationFactName =        "radiation";
 
 TunnelingDataFactGroup::TunnelingDataFactGroup(QObject* parent)
     : FactGroup(1000, ":/json/Vehicle/TunnelingDataFact.json", parent)
     , _temperatureFact  (0, _temperatureFactName,   FactMetaData::valueTypeDouble)
     , _humidityFact     (0, _humidityFactName,      FactMetaData::valueTypeDouble)
+    , _pressureFact     (0, _pressureFactName,      FactMetaData::valueTypeDouble)
     , _windspeedFact    (0, _windspeedFactName,     FactMetaData::valueTypeDouble)
     , _winddirectionFact(0, _winddirectionFactName, FactMetaData::valueTypeDouble)
-    , _pm2_5Fact        (0, _pm2_5FactName,         FactMetaData::valueTypeDouble)
+    , _pm1p0Fact        (0, _pm1p0FactName,         FactMetaData::valueTypeDouble)
+    , _pm2p5Fact        (0, _pm2p5FactName,         FactMetaData::valueTypeDouble)
     , _pm10Fact         (0, _pm10FactName,          FactMetaData::valueTypeDouble)
-    , _radiationFact    (0, _radiationFactName,     FactMetaData::valueTypeDouble)
 {
     _addFact(&_temperatureFact,     _temperatureFactName);
     _addFact(&_humidityFact,        _humidityFactName);
+    _addFact(&_pressureFact,        _pressureFactName);
     _addFact(&_windspeedFact,       _windspeedFactName);
     _addFact(&_winddirectionFact,   _winddirectionFactName);
-    _addFact(&_pm2_5Fact,           _pm2_5FactName);
+    _addFact(&_pm1p0Fact,           _pm1p0FactName);
+    _addFact(&_pm2p5Fact,           _pm2p5FactName);
     _addFact(&_pm10Fact,            _pm10FactName);
-    _addFact(&_radiationFact,       _radiationFactName);
 }
 
 void TunnelingDataFactGroup::handleMessage(Vehicle* vehicle, mavlink_message_t& message)
@@ -48,18 +51,19 @@ void TunnelingDataFactGroup::_handleTunnel(mavlink_message_t &message)
     mavlink_msg_tunnel_decode(&message, &tunnel);
 
     switch(tunnel.payload_type){
-        case 2:
+        case 0:
             struct tunneling_Payload tP;
 
             memcpy(&tP, &tunnel.payload, sizeof(tP));
 
-            _temperatureFact.setRawValue(tP.temperatureRaw/10.f);
-            _humidityFact.setRawValue(tP.humidityRaw/10.f);
+            _temperatureFact.setRawValue(tP.temperatureRaw/100.f);
+            _humidityFact.setRawValue(tP.humidityRaw/100.f);
+            _pressureFact.setRawValue(tP.pressureRaw);
             _windspeedFact.setRawValue(tP.windspeedRaw/10.f);
             _winddirectionFact.setRawValue(tP.winddirectionRaw);
-            _pm2_5Fact.setRawValue(tP.pm2_5Raw/10.f);
+            _pm1p0Fact.setRawValue(tP.pm1p0Raw/10.f);
+            _pm2p5Fact.setRawValue(tP.pm2p5Raw/10.f);
             _pm10Fact.setRawValue(tP.pm10Raw/10.f);
-            _radiationFact.setRawValue(tP.radiationRaw);
 
             break;
     }
--- src/Vehicle/TunnelingDataFactGroup.h
+++ src/Vehicle/TunnelingDataFactGroup.h
@@ -11,49 +11,54 @@ public:
 
     Q_PROPERTY(Fact* temperature    READ temperature     CONSTANT)
     Q_PROPERTY(Fact* humidity       READ humidity        CONSTANT)
+    Q_PROPERTY(Fact* pressure       READ pressure        CONSTANT)
     Q_PROPERTY(Fact* windspeed      READ windspeed       CONSTANT)
     Q_PROPERTY(Fact* winddirection  READ winddirection   CONSTANT)
-    Q_PROPERTY(Fact* pm2_5          READ pm2_5           CONSTANT)
+    Q_PROPERTY(Fact* pm1p0          READ pm1p0           CONSTANT)
+    Q_PROPERTY(Fact* pm2p5          READ pm2p5           CONSTANT)
     Q_PROPERTY(Fact* pm10           READ pm10            CONSTANT)
-    Q_PROPERTY(Fact* radiation      READ radiation       CONSTANT)
 
     Fact* temperature                 () { return &_temperatureFact; }
     Fact* humidity                    () { return &_humidityFact; }
+    Fact* pressure                    () { return &_pressureFact; }
     Fact* windspeed                   () { return &_windspeedFact; }
     Fact* winddirection               () { return &_winddirectionFact; }
-    Fact* pm2_5                       () { return &_pm2_5Fact; }
+    Fact* pm1p0                       () { return &_pm1p0Fact; }
+    Fact* pm2p5                       () { return &_pm2p5Fact; }
     Fact* pm10                        () { return &_pm10Fact; }
-    Fact* radiation                   () { return &_radiationFact; }
 
     // Overrides from FactGroup
     void handleMessage(Vehicle* vehicle, mavlink_message_t& message) override;
 
     static const char* _temperatureFactName;
     static const char* _humidityFactName;
+    static const char* _pressureFactName;
     static const char* _windspeedFactName;
     static const char* _winddirectionFactName;
-    static const char* _pm2_5FactName;
+    static const char* _pm1p0FactName;
+    static const char* _pm2p5FactName;
     static const char* _pm10FactName;
-    static const char* _radiationFactName;
 
 private:
     void _handleTunnel              (mavlink_message_t& message);
 
     Fact _temperatureFact;
     Fact _humidityFact;
+    Fact _pressureFact;
     Fact _windspeedFact;
     Fact _winddirectionFact;
-    Fact _pm2_5Fact;
+    Fact _pm1p0Fact;
+    Fact _pm2p5Fact;
     Fact _pm10Fact;
-    Fact _radiationFact;
 
     struct tunneling_Payload {
         int16_t temperatureRaw;
         uint16_t humidityRaw;
+        float pressureRaw;
         uint16_t windspeedRaw;
         uint16_t winddirectionRaw;
-        uint16_t pm2_5Raw;
+        uint16_t pm1p0Raw;
+        uint16_t pm2p5Raw;
         uint16_t pm10Raw;
-        float radiationRaw;
     };
 };
