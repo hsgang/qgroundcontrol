0051f66a01258f2fcd8bdb5d65a3cd9f7fb667b4
hsgang <hsgangmain@naver.com>
2025-10-17 14:13:03 +0900

    rework android fullscreen

--- src/QGCApplication.h
+++ src/QGCApplication.h
@@ -78,9 +78,6 @@ public:
 
     void setLanguage();
     QQuickWindow *mainRootWindow();
-#ifdef Q_OS_ANDROID
-    void setupAndroidFullScreen(QQuickWindow* window);
-#endif
     uint64_t msecsSinceBoot() const { return _msecsElapsedTime.elapsed(); }
     QString numberToString(quint64 number);
     QString bigSizeToString(quint64 size);
